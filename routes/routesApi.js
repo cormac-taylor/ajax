7a5fec8817ca031c64fea3bc0b90fa900ee12a20
cormactaylor <cormacmtaylor@gmail.com>
2024-12-02 18:28:34 -0500

    inital commit

---
+++ routes/routesApi.js
@@ -0,0 +1,6 @@
+// Set-Up Routes
+
+router.route('/').get(async (req, res) => {
+  //code here for GET to show static HTML flie
+  //IF you have any other routes besides this one, you will get a 0. everything must be done via client-side and AJAX requests (a client-side fetch or axios request can be used instead of AJAX)
+});
