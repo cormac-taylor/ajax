cbcd08448cd0a13e7f8885d1ac2622f35218b75b
cormactaylor <cormacmtaylor@gmail.com>
2024-12-12 19:18:35 -0500

    movie details done

--- public/js/main.js
+++ public/js/main.js
@@ -51,8 +51,11 @@
 
     const movieId = $(this).attr("data-id");
     getMovie(movieId).then(function (details) {
-      console.log(details);
+      $("#movieDetails").append(movieDetailsHtml(details));
     });
+
+    movieDetails.show();
+    return;
   });
 
   async function searchMovies() {
@@ -96,6 +99,160 @@
     }
   }
 })(jQuery);
+
+const movieDetailsHtml = (movieDetailsObj) => {
+  const notAvailable = "N/A";
+
+  let movieTitle;
+  let moviePosterAlt;
+  if (movieDetailsObj.Title) {
+    movieTitle = movieDetailsObj.Title.trim();
+    moviePosterAlt = `${movieTitle} Poster`;
+  } else {
+    movieTitle = notAvailable;
+    moviePosterAlt = notAvailable;
+  }
+
+  let moviePosterSrc;
+  if (movieDetailsObj.Poster && movieDetailsObj.Poster.trim() !== notAvailable) {
+    moviePosterSrc = movieDetailsObj.Poster.trim();
+  } else {
+    moviePosterSrc = "/public/no_image.jpeg";
+    moviePosterAlt = notAvailable;
+  }
+
+  let moviePlot;
+  if (movieDetailsObj.Plot) {
+    moviePlot = movieDetailsObj.Plot.trim();
+  } else {
+    moviePlot = notAvailable;
+  }
+
+  let yearReleased;
+  if (movieDetailsObj.Year) {
+    yearReleased = movieDetailsObj.Year.trim();
+  } else {
+    yearReleased = notAvailable;
+  }
+
+  let movieRating;
+  if (movieDetailsObj.Rated) {
+    movieRating = movieDetailsObj.Rated.trim();
+  } else {
+    movieRating = notAvailable;
+  }
+
+  let movieRuntime;
+  if (movieDetailsObj.Runtime) {
+    movieRuntime = movieDetailsObj.Runtime.trim();
+  } else {
+    movieRuntime = notAvailable;
+  }
+
+  let movieGenres;
+  if (movieDetailsObj.Genre) {
+    movieGenres = movieDetailsObj.Genre.trim();
+  } else {
+    movieGenres = notAvailable;
+  }
+
+  let boxOfficeEarnings;
+  if (movieDetailsObj.BoxOffice) {
+    boxOfficeEarnings = movieDetailsObj.BoxOffice.trim();
+  } else {
+    boxOfficeEarnings = notAvailable;
+  }
+
+  let dvdReleaseDate;
+  if (movieDetailsObj.DVD) {
+    dvdReleaseDate = movieDetailsObj.DVD.trim();
+  } else {
+    dvdReleaseDate = notAvailable;
+  }
+
+  let movieDirector;
+  if (movieDetailsObj.Director) {
+    movieDirector = movieDetailsObj.Director.trim();
+  } else {
+    movieDirector = notAvailable;
+  }
+
+  let movieWriter;
+  if (movieDetailsObj.Writer) {
+    movieWriter = movieDetailsObj.Writer.trim();
+  } else {
+    movieWriter = notAvailable;
+  }
+
+  let movieCast;
+  if (movieDetailsObj.Actors) {
+    movieCast = movieDetailsObj.Actors.trim();
+  } else {
+    movieCast = notAvailable;
+  }
+
+  let ratingsTableValues = "";
+  if (movieDetailsObj.Ratings) {
+    for (let rating of movieDetailsObj.Ratings) {
+      ratingsTableValues = ratingsTableValues.concat(`
+                <tr>
+                    <td>${rating.Source.trim()}</td>
+                    <td>${rating.Value.trim()}</td>
+                </tr>`);
+    }
+  }
+
+  const movieDetailsHtml = `
+<article>
+  <h1>${movieTitle}</h1>
+  <img
+    alt="${moviePosterAlt}"
+    src="${moviePosterSrc}"
+  />
+
+  <h2>Plot</h2>
+  <p>
+    ${moviePlot}
+  </p>
+  <section>
+    <h3>Info</h3>
+    <dl>
+      <dt>Year Released:</dt>
+      <dd>${yearReleased}</dd>
+      <dt>Rated:</dt>
+      <dd>${movieRating}</dd>
+      <dt>Runtime:</dt>
+      <dd>${movieRuntime}</dd>
+      <dt>Genre(s):</dt>
+      <dd>${movieGenres}</dd>
+      <dt>Box Office Earnings:</dt>
+      <dd>${boxOfficeEarnings}</dd>
+      <dt>DVD Release Date:</dt>
+      <dd>${dvdReleaseDate}</dd>
+    </dl>
+  </section>
+
+  <section>
+    <h4>Cast and Crew</h4>
+    <p><strong>Director:</strong> ${movieDirector}</p>
+    <p><strong>Writer:</strong> ${movieWriter}</p>
+    <p><strong>Cast:</strong> ${movieCast}</p>
+  </section>
+
+  <section>
+    <h4>Ratings</h4>
+    <table class="my_coolratings_table">
+      <tr>
+        <th>Source</th>
+        <th>Rating</th>
+      </tr>
+      ${ratingsTableValues}
+    </table>
+  </section>
+</article>
+`;
+  return movieDetailsHtml;
+};
 
 const validateString = (str) => {
   if (typeof str !== "string") throw "must be a string.";
